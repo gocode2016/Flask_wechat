e4a21f1df6d3081d5ee41ffac636ba929ee2db93
jack891011 <545943059@qq.com>
2018-08-05 22:33:44 +0800

    access_token 没2小时记录文件一次

--- access_token.py
+++ access_token.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import requests
-
+import time
+import schedule
+import datetime
 Token = "sdfrhgcfb452135"
 Appid = "wx1faaf9bd50cec162"
 Appsecret = "fbff6ed16c87fdbce8dce1dbaf316624"
 
-access_token = requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (Appid,Appsecret)).json()
-print(access_token)
+
+def get_access_token():
+    at = time.time()
+    access_token = requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (Appid,Appsecret)).json()
+    with open('access_token.txt','w') as f:
+        print(access_token,file=f)
+    return access_token
+
+if __name__=='__main__':
+
+    schedule.every(120).minutes.do(get_access_token())
+    while True:                                         ### 每30秒检查一次时间
+        schedule.run_pending()
+        print(datetime.datetime.now())
+        time.sleep(30)
